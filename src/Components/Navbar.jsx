6394a551a9fce4c6be7c57d15e8d5255ffc78f6d
Samuel Rodriguez <samuelrdg11@gmail.com>
2023-01-15 09:40:32 -0500

    Limpieza del proyecto, creacion de componente Navbar junto a su css

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className='navbar mb-2'>
+            <h5 className='mx auto' id='tituloprincipa'>Aplicación del Clima</h5>
+        </div>
+        <p>
+            <i className='lead'>Habilita la localización de tu navegador</i>
+        </p>
+    </div>
+  )
+}
+
+export default Navbar
