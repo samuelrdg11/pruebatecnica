78f06a2a6f5d103a519c4c020b0e720660d27545
Samuel Rodriguez <samuelrdg11@gmail.com>
2023-01-15 11:02:38 -0500

    Creacion de la funcion busqueda del clima de la ciudad, hooks necesarios y de un input para recibir dato, prueba de funcionalidad en la consola

--- src/Components/Buscador.js
+++ src/Components/Buscador.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 const Buscador = () => {
+
+    const[ciudad, setCiudad] = useState("");
+    const[clima, setClima] = useState("");
+    
+
+    const ubicacionURL = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&lang=es&units=metric&appid=251643f089f7a4591111863c7f26cb76`
 
     const localizacion = () => {
         navigator.geolocation.getCurrentPosition(getUbicacionActual)
@@ -14,10 +21,35 @@ const Buscador = () => {
             console.log(data)
         })
     }
+
+    const getClimaInput = async () => {
+        const fetchClimaInput = await fetch(ubicacionURL)
+        await fetchClimaInput.json()
+        .then (data => {
+            console.log(data)
+        })
+    }
+
+    useEffect(() => {
+        getClimaInput();
+    },[ciudad])
+
+    const busquedaClimatica = (e) => {
+        if (e.key === "Enter"){
+            setCiudad(e.target.value)
+        }
+    }
+
     return (
-        <div>
+        <div className='App'>
             <button onClick={localizacion}> Obtener Ubicacion</button>
+            <input 
+            type="text"
+            className='form-control'
+            onKeyDown={busquedaClimatica}
+            />
         </div>
+
     )
 }
 
